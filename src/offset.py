57c31183bc6f0b724581b9b5a96a22f7813c9fec
Rolf Piepenbrink <rolf.piep.mobiel@gmail.com>
2023-10-12 11:22:10 +0200

    Update comments in offset

--- src/offset.py
+++ src/offset.py
@@ -13,10 +13,12 @@ class DimensionsNotSupported(Exception):
 
 def getOffsets(cardinal: bool = True, dimensions: int = 2):
     if dimensions > 3 or dimensions < 1:
-        raise DimensionsNotSupported(f"Number of dimensions is not support. Dimension should not be less than 1 and should not exceed 3. Got: {dimensions}")
+        raise DimensionsNotSupported(f"Dimension should not be less than 1 and should not exceed 3. Got: {dimensions}")
     if cardinal:
-        # There are two neighbours per dimension.
+        # There are two neighbours to consider per dimension.
         offsets = [[0 for _ in range(dimensions)] for _ in range(dimensions * 2)]
+        
+        # By taking the ith position, cardinality is ensured, adhering to the format [0, {-1,1}] (and variations thereof).
         for i in range(dimensions):
             offsets[2*i][i] = -1
             offsets[2*i+1][i] = 1 
