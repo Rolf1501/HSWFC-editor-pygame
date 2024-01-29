cd795de46b2133db6a84dfb762cb8d9f873b2745
Rolf Piepenbrink <rolf.piep.mobiel@gmail.com>
2024-01-29 15:28:37 +0100

    Determine masks for all distinct orientations of a terminal

--- src/terminal.py
+++ src/terminal.py
@@ -12,71 +12,97 @@ from util_data import Dimensions as D, Cardinals as C, Colour
 @dataclass
 class Terminal:
     extent: BB  # BB with extent relative to grid units
-    symmetry_axes: dict[D, set[D]]
     colour: Colour
     up: C = field(default=C.TOP)
-    orientation: C = field(default=C.NORTH)
+    # orientation: C = field(default=C.NORTH)
     mask: np.ndarray = field(default=None)
-    unique_orientations: int = field(init=False)
+
+    # Specifies how the terminal may be oriented; default 0 means that the terminal may only point North.
+    distinct_orientations: list[int] = field(default=[0])
     atom_indices: np.ndarray = field(init=False)
-    atom_mask: np.ndarray = field(init=False)
-    atom_coord_mask: np.ndarray = field(init=False)
+    # atom_mask: np.ndarray = field(init=False)
     heightmaps: dict = field(init=False)
     n_atoms: int = field(init=False)
     atom_index_to_id_mapping: dict[Coord, Atom] = field(init=False)
+    oriented_mask: dict[int, np.ndarray] = field(init=False)
+    oriented_atom_mask: dict[int, np.ndarray] = field(init=False)
+    oriented_indices: dict[int, np.ndarray] = field(init=False)
+    oriented_heightmaps: dict = field(init=False)
 
     def __post_init__(self):
+        self.atom_index_to_id_mapping = {}
         if self.mask is None:
             self.mask = np.full(self.extent.whd(), True)
 
-        self.atom_index_to_id_mapping = {}
-        # Find all cells in the mask that are not empty, these are the atoms uniquely identified by their index.
-        non_empty_cells = np.transpose(self.mask.nonzero())
-        self.atom_indices = [Coord(xyz[1], xyz[0], xyz[2]) for xyz in non_empty_cells]
-
-        whd = self.extent.whd()
-        self.atom_mask = np.full((whd.y, whd.x, whd.z, len(self.atom_indices)), False)
-        self.atom_coord_mask = np.full((whd.y, whd.x, whd.z), None)
-
-        self.n_atoms = len(self.atom_indices)
-
-        # Set the corresponding atoms' cells to True.
-        for i in range(len(self.atom_indices)):
-            c = self.atom_indices[i]
-            curr = self.atom_mask[c.y, c.x, c.z]
-            self.atom_coord_mask[c.y, c.x, c.z] = Coord(c.x, c.y, c.z)
-            curr[i] = True
-
-        # Determine which atom index requires which specific atom model.
-        for i in self.atom_indices:
-            x, y, z = i
-            bottom = True if y - 1 > 0 and self.mask[y - 1, x, z] else False
-            top = True if y + 1 < whd.y and self.mask[y + 1, x, z] else False
-            west = True if x - 1 > 0 and self.mask[y, x - 1, z] else False
-            east = True if x + 1 < whd.x and self.mask[y, x + 1, z] else False
-            south = True if z - 1 > 0 and self.mask[y, x, z - 1] else False
-            north = True if z + 1 < whd.z and self.mask[y, x, z + 1] else False
-            sd = SD(bottom, top, west, east, south, north)
-            self.atom_index_to_id_mapping[i] = Atom(sd)
+        self.calc_oriented_masks()
 
         self.calc_heightmaps()
+
+    def calc_atom_indices(self, mask: np.ndarray):
+        # Find all cells in the mask that are not empty, these are the atoms uniquely identified by their index.
+        non_empty_cells = np.transpose(mask.nonzero())
+        indices = [Coord(xyz[1], xyz[0], xyz[2]) for xyz in non_empty_cells]
+        return indices
+
+    def calc_oriented_masks(self):
+        self.oriented_mask = {}
+        whd = self.extent.whd()
+
+        self.n_atoms = 0
+        for o in self.distinct_orientations:
+            # The orientation determines how many counter clockwise rotations of 90 degrees are needed.
+            self.oriented_mask[o] = np.rot90(self.mask, o, axes=(0, 1))
+            indices = self.calc_atom_indices(o, self.oriented_mask[o])
+            self.oriented_indices[o] = indices
+
+            self.oriented_atom_mask[o] = np.full(
+                (whd.y, whd.x, whd.z, len(indices)), False
+            )
+            self.n_atoms += len(indices)
+
+            # Set the corresponding atoms' cells to True.
+            for i in range(len(indices)):
+                c = indices[i]
+                curr = self.oriented_atom_mask[o][c.y, c.x, c.z]
+                curr[i] = True
+
+            # Determine which atom index requires which specific atom model.
+            for i in self.atom_indices:
+                x, y, z = i
+                bottom = True if y - 1 > 0 and self.mask[y - 1, x, z] else False
+                top = True if y + 1 < whd.y and self.mask[y + 1, x, z] else False
+                west = True if x - 1 > 0 and self.mask[y, x - 1, z] else False
+                east = True if x + 1 < whd.x and self.mask[y, x + 1, z] else False
+                south = True if z - 1 > 0 and self.mask[y, x, z - 1] else False
+                north = True if z + 1 < whd.z and self.mask[y, x, z + 1] else False
+                sd = SD(bottom, top, west, east, south, north)
+                self.atom_index_to_id_mapping[i] = Atom(sd)
 
     def calc_heightmaps(self):
         """
         Calculates the distance along each axis to the first occupied cell.
         """
         self.heightmaps = {}
-        # TODO: verify whether the mapping of axis to cardinal is correct.
+        self.oriented_heightmaps = {}
+
         axes = {
             0: (Offset.from_cardinal(C.BOTTOM), Offset.from_cardinal(C.TOP)),
             1: (Offset.from_cardinal(C.WEST), Offset.from_cardinal(C.EAST)),
             2: (Offset.from_cardinal(C.SOUTH), Offset.from_cardinal(C.NORTH)),
         }
-        for axis in axes.keys():
-            cardinal_along, cardinal_against = axes[axis]
-            hm_along, hm_against = self.calc_heightmap_for_axis(self.mask, axis=axis)
-            self.heightmaps[cardinal_along] = hm_along
-            self.heightmaps[cardinal_against] = hm_against
+        for o in self.distinct_orientations:
+            for axis in axes.keys():
+                cardinal_along, cardinal_against = axes[axis]
+                # hm_along, hm_against = self.calc_heightmap_for_axis(
+                #     self.mask, axis=axis
+                # )
+                hm_along_orr, hm_against_orr = self.calc_heightmap_for_axis(
+                    self.oriented_mask[o], axis=axis
+                )
+                self.oriented_heightmaps[o][cardinal_along] = hm_along_orr
+                self.oriented_heightmaps[o][cardinal_against] = hm_against_orr
+                # self.heightmaps[cardinal_along] = hm_along
+                # self.heightmaps[cardinal_against] = hm_against
 
     def calc_heightmap_for_axis(self, mask: np.ndarray, axis):
         """
