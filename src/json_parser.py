48999f074aec8ce4f9c665ee676ce9eaee69042b
Rolf Piepenbrink <rolf.piep.mobiel@gmail.com>
2024-02-03 17:14:17 +0100

    Add jsons and order code

--- src/json_parser.py
+++ src/json_parser.py
@@ -16,42 +16,12 @@ class JSONParser:
     def __init__(self) -> None:
         self.terminal_file = "terminals.json"
         self.terminal_root = "terminals"
-        self.examples_file = "example.json"
+        self.examples_file = "examples.json"
         self.json_folder = "json"
         self.json_path = self.get_src_dir().joinpath(self.json_folder)
         self.terminal_path = self.json_path.joinpath(self.terminal_file)
         self.examples_path = self.json_path.joinpath(self.examples_file)
         self._init_file(self.terminal_path)
-
-    def get_src_dir(self):
-        return Path(__file__).parent
-
-    def write_to_json(self, content, path: Path):
-        with open(path, "w") as file:
-            json.dump(content, file, indent=2)
-
-    def append_terminal(self, new_terminal: Terminal, terminal_id):
-        with open(self.terminal_path) as file:
-            curr_data = json.load(file)
-            curr_data[str(terminal_id)] = new_terminal.to_json()
-            self.write_to_json(curr_data, self.terminal_path)
-
-    def write_example(self, example: Example):
-        with open(self.examples_path) as file:
-            curr_data = json.load(file)
-            curr_data[example.name] = example.to_json()
-            print("TYPE: ", type(curr_data))
-            self.write_to_json(curr_data, self.examples_path)
-
-    def update_example_adjacency(self, adjacency: list[Adjacency], example_name):
-        with open(self.examples_path) as file:
-            curr_data = json.load(file)
-            if not example_name in curr_data or not isinstance(
-                curr_data[example_name], list
-            ):
-                curr_data[example_name] = []
-            curr_data[example_name].append(adjacency.to_json())
-            self.write_to_json(curr_data, self.examples_path)
 
     def _init_file(self, path: Path):
         if not exists(path.parent):
@@ -66,6 +36,29 @@ class JSONParser:
     def _reset_file(self, path):
         with open(path, "w") as file:
             json.dump({}, file)
+
+    def get_src_dir(self):
+        return Path(__file__).parent
+
+    def write_to_json(self, content, path: Path):
+        with open(path, "w") as file:
+            json.dump(content, file, indent=2)
+
+    def append_terminal(self, new_terminal: Terminal, terminal_id):
+        with open(self.terminal_path) as file:
+            curr_data = json.load(file)
+            curr_data[str(terminal_id)] = new_terminal.to_json()
+            self.write_to_json(curr_data, self.terminal_path)
+
+    def update_example_adjacency(self, adjacency: list[Adjacency], example_name):
+        with open(self.examples_path) as file:
+            curr_data = json.load(file)
+            if not example_name in curr_data or not isinstance(
+                curr_data[example_name], list
+            ):
+                curr_data[example_name] = []
+            curr_data[example_name].append(adjacency.to_json())
+            self.write_to_json(curr_data, self.examples_path)
 
     def read_terminals(self):
         terminals = {}
@@ -101,6 +94,12 @@ class JSONParser:
     def write_terminals(self, terminals: list[Terminal]):
         for t in terminals:
             self.append_terminal(terminals[t], t)
+
+    def write_example(self, example: Example):
+        with open(self.examples_path) as file:
+            curr_data = json.load(file)
+            curr_data[example.name] = example.to_json()
+            self.write_to_json(curr_data, self.examples_path)
 
 
 jsonparser = JSONParser()
