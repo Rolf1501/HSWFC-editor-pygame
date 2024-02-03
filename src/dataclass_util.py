5eb1c6dd70becf63e1154647898f103b7520e38d
Rolf Piepenbrink <rolf.piep.mobiel@gmail.com>
2024-02-03 17:09:29 +0100

    Support transforming dictionary into dataclass instance

--- src/dataclass_util.py
+++ src/dataclass_util.py
@@ -4,3 +4,8 @@ from dataclasses import fields, is_dataclass
 def get_init_field_names(cls):
     if is_dataclass(cls):
         return [f.name for f in fields(cls) if f.init]
+
+
+def dict_to_dataclass_instance(cls, dct: dict):
+    fields = get_init_field_names(cls)
+    return cls(**{f: dct[f] for f in fields})
